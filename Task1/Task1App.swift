be635cec721afdca4c74dd82a14de0479e828412
sasasan03 <sako.hiro62@gmail.com>
2022-09-17 17:52:18 +0900

    Initial Commit

---
+++ Task1/Task1App.swift
@@ -0,0 +1,17 @@
+//
+//  Task1App.swift
+//  Task1
+//
+//  Created by sako0602 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct Task1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
